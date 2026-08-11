00558a51172b40d011ebe770dc8436f07e7c0954
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2026-08-11 16:22:14 +0600

    ai style modify

--- src/App.jsx
+++ src/App.jsx
@@ -164,13 +164,9 @@ export default function App({ hostElement = null }) {
           title="EnoXAI"
         >
           {isOpen ? (
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-              <polyline points="6 9 12 15 18 9" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="none" stroke="#767676" stroke-width="2" d="m2 8.35l10.173 9.823L21.997 8"/></svg>
           ) : (
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#767676" d="M1.5 2h21v16H6.876L1.5 22.704V2Zm2 2v14.296L6.124 16H20.5V4h-17Z"></path></svg>
           )}
         </button>
       </div>
